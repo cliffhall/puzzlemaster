d27e0cd8c1de04f07d70c69170f1145565053722
cliffhall <cliff@futurescale.com>
2025-04-12 15:31:05 -0400

    initial commit

---
+++ src/preload/index.ts
@@ -0,0 +1,33 @@
+import { contextBridge } from 'electron'
+import { ElectronAPI, electronAPI } from '@electron-toolkit/preload'
+
+export interface API {
+  marco: () => string
+}
+
+declare global {
+  interface Window {
+    electron: ElectronAPI
+    api: API
+  }
+}
+
+// Custom APIs for renderer
+const api: API = {
+  marco: () => 'polo'
+}
+
+// Use `contextBridge` APIs to expose Electron APIs to
+// renderer only if context isolation is enabled, otherwise
+// just add to the DOM global.
+if (process.contextIsolated) {
+  try {
+    contextBridge.exposeInMainWorld('electron', electronAPI)
+    contextBridge.exposeInMainWorld('api', api)
+  } catch (error) {
+    console.error(error)
+  }
+} else {
+  window.electron = electronAPI
+  window.api = api
+}
